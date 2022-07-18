c6ce195a45abe405533b27508646daa75f6e9ced
mcst12345 <herobrinetlb@gmail.com>
2022-07-18 21:32:25 +0800

    Changes

--- src/main/java/miku/miku/Miku.java
+++ src/main/java/miku/miku/Miku.java
@@ -37,7 +37,7 @@ public class Miku {
 
     public static final String MODID = "miku";
     public static final String NAME = "Miku";
-    public static final String VERSION = "1.0.1-pre7";
+    public static final String VERSION = "1.0.1-pre8";
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) throws IOException {
