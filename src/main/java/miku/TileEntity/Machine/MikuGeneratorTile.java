5fb8df9ecefb4ef44760955443226b31260b7af3
mcst12345 <herobrinetlb@gmail.com>
2022-12-16 12:45:32 +0800

    Changes

--- src/main/java/miku/TileEntity/Machine/MikuGeneratorTile.java
+++ src/main/java/miku/TileEntity/Machine/MikuGeneratorTile.java
@@ -13,14 +13,20 @@ public class MikuGeneratorTile extends MikuPowerTileBase {
     @Override
     public void update() {
         if (power >= MaxPower) return;
-        IInventory inventory = TileEntityHopper.getInventoryAtPosition(world, pos.getX(), pos.getY() + 1, pos.getZ());
-        if (inventory != null) {
-            for (int index = 0; index < inventory.getSizeInventory(); ++index) {
-                ItemStack itemstack = inventory.getStackInSlot(index);
-                if (itemstack.getItem() instanceof Scallion) {
-                    if (itemstack.getCount() > 0) {
-                        power++;
-                        itemstack.shrink(1);
+        for (int x = pos.getX() - 10; x <= pos.getX() + 10; x++) {
+            for (int y = pos.getY() - 10; y <= pos.getY() + 10; y++) {
+                for (int z = pos.getZ() - 10; z <= pos.getZ() + 10; z++) {
+                    IInventory inventory = TileEntityHopper.getInventoryAtPosition(world, x, y, z);
+                    if (inventory != null) {
+                        for (int index = 0; index < inventory.getSizeInventory(); ++index) {
+                            ItemStack itemstack = inventory.getStackInSlot(index);
+                            if (itemstack.getItem() instanceof Scallion) {
+                                if (itemstack.getCount() > 0) {
+                                    power++;
+                                    itemstack.shrink(1);
+                                }
+                            }
+                        }
                     }
                 }
             }
