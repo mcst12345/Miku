ddb5a07cbf2ee141e4cc619ade52fb166318a1b3
mcst12345 <herobrinetlb@gmail.com>
2022-12-16 12:23:23 +0800

    Changes

--- src/main/java/miku/TileEntity/Machine/MikuRFConverterTile.java
+++ src/main/java/miku/TileEntity/Machine/MikuRFConverterTile.java
@@ -1,5 +1,7 @@
 package miku.TileEntity.Machine;
 
+import cofh.redstoneflux.api.IEnergyReceiver;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.common.Loader;
 
 public class MikuRFConverterTile extends MachineTileBase {
@@ -11,5 +13,19 @@ public class MikuRFConverterTile extends MachineTileBase {
     public void update() {
         super.update();
         if (!Loader.isModLoaded("redstoneflux") || power < 1) return;
+        for (int x = pos.getX() - 5; x <= pos.getX() + 5; x++) {
+            for (int y = pos.getY() - 5; y <= pos.getY() + 5; y++) {
+                for (int z = pos.getZ() - 5; z <= pos.getZ() + 5; z++) {
+                    if (world.getTileEntity(new BlockPos(x, y, z)) instanceof IEnergyReceiver) {
+                        IEnergyReceiver energyReceiver = (IEnergyReceiver) world.getTileEntity(new BlockPos(x, y, z));
+                        if (energyReceiver != null) {
+                            power--;
+                            int received = energyReceiver.receiveEnergy(null, 1, false);
+                            if (received != 1) power++;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
