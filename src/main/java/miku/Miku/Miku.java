6ea3822858389adb749415297a3446f228180e30
root <herobrinetlb@gmail.com>
2023-08-06 11:10:29 +0800

    Changes

--- src/main/java/miku/Miku/Miku.java
+++ src/main/java/miku/Miku/Miku.java
@@ -26,7 +26,7 @@ public class Miku {
     public static final String MODID = "miku";
 
     public static final String NAME = "Miku";
-    public static final String VERSION = "1.1.6";
+    public static final String VERSION = "1.1.7";
 
     public Miku() {
     }
